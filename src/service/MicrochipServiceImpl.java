3e4bb68c2692b696c74f0f2f73c593d85ffe14e4
YOHVASS <yovassphoto@gmail.com>
2025-11-14 12:51:29 -0300

    Update MicrochipServiceImpl.java

--- src/service/MicrochipServiceImpl.java
+++ src/service/MicrochipServiceImpl.java
@@ -4,6 +4,12 @@
  */
 package service;
 
-public class MicrochipServiceImpl {
-    
-}
+import java.util.List;
+
+public interface GenericService<T> {
+    void insertar(T entidad) throws Exception;
+    void actualizar(T entidad) throws Exception;
+    void eliminar(int id) throws Exception;
+    T getById(int id) throws Exception;
+    List<T> getAll() throws Exception;
+    }
